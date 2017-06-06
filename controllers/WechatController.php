1c39f24df4f9f4390d39fafb1ccc1cde1eb15a09
xiaohaoyong <xiaohaoyong@126.com>
2017-06-06 09:22:46 +0800

    个人中心

--- controllers/WechatController.php
+++ controllers/WechatController.php
@@ -31,17 +31,15 @@ class WechatController extends Controller
     public function actionSetmenu()
     {
 
-
-        $contentUrl = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . WECHAT_APPID . '&redirect_uri=' . $contentViews . '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+        $user_index = urlencode(Url::to('user/index',["t"=>time()]));
+        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . WECHAT_APPID . '&redirect_uri=' . $user_index . '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
 
         $menuList = array(
             array('id' => '1', 'pid' => '0', 'name' => '拉手学院', 'type' => 'view', 'code' => 'http://www.wedoctors.com.cn/down/app.html'),
             array('id' => '2', 'pid' => '0', 'name' => '全科学院', 'type' => 'view', 'code' => 'http://www.wedoctors.com.cn/down/app.html'),
             array('id' => '3', 'pid' => '0', 'name' => '我的', 'type' => 'view', 'code' =>''),
             array('id' => '4', 'pid' => '3', 'name' => '下载APP', 'type' => 'view', 'code' =>'http://www.wedoctors.com.cn/down/app.html'),
-            array('id' => '5', 'pid' => '3', 'name' => '个人中心', 'type' => 'view', 'code' =>"http://www.wedoctors.com.cn/down/app.html"),
-
-
+            array('id' => '5', 'pid' => '3', 'name' => '个人中心', 'type' => 'view', 'code' =>$url),
         );
         print_r($menuList);
         $result = Menu::setMenu($menuList);
